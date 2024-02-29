d32b7e2702593c4ab79e7f05ff78f4f99c7672ae
Abeer Saxena <65819026+AbeerSaxena@users.noreply.github.com>
2024-03-01 00:18:18 +0530

    Update TestMedicureService.java

--- src/test/java/com/project/staragile/TestMedicureService.java
+++ src/test/java/com/project/staragile/TestMedicureService.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class TestMedicureService {
 	
 	@Autowired
-	MedicureService doctorService;
+	MedicureService doctorService; 
 	
 	@Test
 	public void testRegisterDoctor() {
