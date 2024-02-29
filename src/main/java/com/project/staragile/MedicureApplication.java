b2c2075de806c5a642b8d17415e4a0b1dfaaa4c1
Abeer Saxena <65819026+AbeerSaxena@users.noreply.github.com>
2024-03-01 00:18:59 +0530

    Update MedicureApplication.java

--- src/main/java/com/project/staragile/MedicureApplication.java
+++ src/main/java/com/project/staragile/MedicureApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedicureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MedicureApplication.class, args);
+		SpringApplication.run(MedicureApplication.class, args); 
 	}
 
 }
